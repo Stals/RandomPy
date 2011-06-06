4085475a66898f99d3a5b57ac49d271f8a00da66
Stals <superstals@gmail.com>
2011-06-06 18:30:19 +0400

    now you can output number of lines

--- randomPy.py
+++ randomPy.py
@@ -12,8 +12,17 @@ word3=f.readlines();
 f = open('numb4.txt','r');
 word4=f.readlines();
 
-#Выводит на печать случайные слова из того списка что мы только что прочитали
-#[:-1] используется для того чтобы не печатать последний символ - которым является символ переноса строки
-print("{0} {1}, который любит {2} {3}.".format(random.choice(word1)[:-1],random.choice(word2)[:-1],random.choice(word3)[:-1],random.choice(word4)[:-1]));
-
 f.close();
+
+
+while True:
+    numberOfLines=input("Веедите кол-во строк которое хотите получить:");
+    print("\n");
+    if numberOfLines.isdigit():
+        i=0
+        while i<int(numberOfLines):
+            #Выводит на печать случайные слова из того списка что мы только что прочитали
+            #[:-1] используется для того чтобы не печатать последний символ - которым является символ переноса строки
+            print("{0} {1}, который любит {2} {3}.".format(random.choice(word1)[:-1],random.choice(word2)[:-1],random.choice(word3)[:-1],random.choice(word4)[:-1]));
+            i=i+1;
+    print("\n");
