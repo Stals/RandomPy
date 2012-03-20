6cd89d7cf9336f6282f3109a4eeaa644c27e8181
Stanislav Korotaev <superstals@gmail.com>
2012-03-20 16:45:38 +0400

    refactored

--- randomPy.py
+++ randomPy.py
@@ -1,23 +1,21 @@
 import random
 
-f = open('word1.txt','r') 
-word1 = f.readlines()
-f = open('word2.txt','r') 
-word2 = f.readlines()
-f = open('word3.txt','r') 
-word3 = f.readlines()
-f = open('word4.txt','r') 
-word4 = f.readlines()
-f.close()
+word1 = open('word1.txt','r').readlines()
+word2 = open('word2.txt','r').readlines()
+word3 = open('word3.txt','r').readlines()
+word4 = open('word4.txt','r').readlines()
 
+# Бесконечный цикл пока не получим число строк
 while True:
     numberOfLines = input("Веедите кол-во строк которое хотите получить:")
-    print("\n")
     if numberOfLines.isdigit():
-        i = 0
-        while i < int(numberOfLines):
-            #Выводит на печать случайные слова из того списка что мы только что прочитали
-            #[:-1] используется для того чтобы не печатать последний символ - которым является символ переноса строки
-            print("{0} {1}, который любит {2} {3}.".format(random.choice(word1)[:-1],random.choice(word2)[:-1],random.choice(word3)[:-1],random.choice(word4)[:-1]))
-            i = i + 1
-    print("\n")
+        break
+
+for i in range(int(numberOfLines)):
+    # Выводит на печать случайные слова из того списка что мы только что прочитали
+    # [:-1] используется для того чтобы не печатать последний символ - которым является символ переноса строки
+    randomWord1 = random.choice(word1)[:-1]
+    randomWord2 = random.choice(word2)[:-1]
+    randomWord3 = random.choice(word3)[:-1]
+    randomWord4 = random.choice(word4)[:-1]
+    print("{0} {1}, который любит {2} {3}.".format(randomWord1, randomWord2, randomWord3, randomWord4))
