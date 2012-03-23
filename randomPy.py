6e9227cef780c902ccf2aae12cae2034fbf65879
Stanislav Korotaev <superstals@gmail.com>
2012-03-23 21:54:53 +0400

    remove [:-1] and now remove newlines while reading just after reading the file

--- randomPy.py
+++ randomPy.py
@@ -1,9 +1,9 @@
 import random
-
-word1 = open('word1.txt','r').readlines()
-word2 = open('word2.txt','r').readlines()
-word3 = open('word3.txt','r').readlines()
-word4 = open('word4.txt','r').readlines()
+# read().splitlines() позволяет получить список строк без '\n' на конце в отличии от просто readlines()
+word1 = open('word1.txt','r').read().splitlines()
+word2 = open('word2.txt','r').read().splitlines()
+word3 = open('word3.txt','r').read().splitlines()
+word4 = open('word4.txt','r').read().splitlines()
 
 # Бесконечный цикл пока не получим число строк
 while True:
@@ -13,9 +13,8 @@ while True:
 
 for i in range(int(numberOfLines)):
     # Выводит на печать случайные слова из того списка что мы только что прочитали
-    # [:-1] используется для того чтобы не печатать последний символ - которым является символ переноса строки
-    randomWord1 = random.choice(word1)[:-1]
-    randomWord2 = random.choice(word2)[:-1]
-    randomWord3 = random.choice(word3)[:-1]
-    randomWord4 = random.choice(word4)[:-1]
+    randomWord1 = random.choice(word1)
+    randomWord2 = random.choice(word2)
+    randomWord3 = random.choice(word3)
+    randomWord4 = random.choice(word4)
     print("{0} {1}, который любит {2} {3}.".format(randomWord1, randomWord2, randomWord3, randomWord4))
